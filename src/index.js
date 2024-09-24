af8293babbe631a39cccbe19ed81d0a9363c4170
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2024-09-24 19:34:33 -0400

    Improve health check endpoint for payload handling

--- src/index.js
+++ src/index.js
@@ -5,8 +5,26 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/healthz', async (req, res) => {
+
     try {
+        console.log(Object.keys(req.query).length);
+        console.log(Object.keys(req.query));
+        console.log(Object);
+        console.log("~~~")
+
+        if(Object.keys(req.query).length>0){
+            return res.status(400)
+            .set('Cache-Control', 'no-cache, no-store, must-revalidate')
+            .set('Pragma','no-cache')
+            .json({
+                status:'Error',
+                message:"The healthcheck API doesn't accept requests with payload."
+            })
+
+        }
         await sequelize.authenticate();  
+
+
         res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
         res.status(200).send();  
     } catch (error) {
