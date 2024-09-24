8f29de4cd2f963d54d2b77314c70a1ccecee5d9c
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2024-09-24 19:50:12 -0400

    Improve healthcheck endpoint error handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sequelize = require('./models/models.js');
+const e = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -7,10 +8,11 @@ const PORT = process.env.PORT || 8080;
 app.get('/healthz', async (req, res) => {
 
     try {
-        console.log(Object.keys(req.query).length);
-        console.log(Object.keys(req.query));
-        console.log(Object);
-        console.log("~~~")
+        // console.log(Object.keys(req.query).length);
+        // console.log(Object.keys(req.query));
+        // console.log(Object);
+        // console.log("~~~")
+        const startTime = Date.now();
 
         if(Object.keys(req.query).length>0){
             return res.status(400)
@@ -24,16 +26,35 @@ app.get('/healthz', async (req, res) => {
         }
         await sequelize.authenticate();  
 
+        const timeduration = Date.now() - startTime
 
-        res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-        res.status(200).send();  
+
+        res.set('Cache-Control', 'no-cache, no-store, must-revalidate')
+        .set('Pragma','no-cache')
+        .status(200)
+        .json({
+            status:'Success',
+            message:"The Application is healthy",
+            responseTime: `${timeduration} ms`
+        })
+        // res.status(200).send();  
     } catch (error) {
-        console.error('Health check failed:', error);  
-        res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-        res.status(503).send();  
+        console.log("---- HEALTH CHECK ERROR STARTS----")
+        console.error('Health check failed:', error.message);  
+        console.log("---- HEALTH CHECK ERROR ENDS----")
+        // console.log(error)
+        // console.log(error.message)
+        res.set('Cache-Control', 'no-cache, no-store, must-revalidate')
+        .set('Pragma','no-cache')
+
+        .status(503)
+        .json({
+            status: 'Error',
+            message:'The healthcheck has failed - Check Db Connection',
+        });
     }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`The Server is running on port https://localhost:${PORT}`);
 });
