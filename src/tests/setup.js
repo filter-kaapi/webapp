029e7d7a7017840f7d7731daf3589cbcad217a21
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2024-10-10 01:18:20 -0400

    chore: fixes to test setup file

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,15 +1,15 @@
 
+if (!process.env.GITHUB_ACTIONS) {
+    if (process.env.NODE_ENV === 'test') {
+        const result = require('dotenv').config({ path: '.env.test' });
 
-if (process.env.NODE_ENV === 'test') {
-    const result = require('dotenv').config({ path: '.env.test' });
-
-    if (result.error) {
-        throw new Error('Could not find .env.test file. Tests cannot run without it.');
+        if (result.error) {
+            throw new Error('Could not find .env.test file. Tests cannot run without it.');
+        }
+    } else {
+        throw new Error(' NODE_ENV is not set to test!');
     }
-} else {
-    throw new Error(' NODE_ENV is not set to test!');
 }
-
 
 
 const sequelize = require('../database/sequelize');
