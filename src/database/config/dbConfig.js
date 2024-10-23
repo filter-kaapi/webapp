535db8ad52f47f3059b493c2dda506733d3b38cb
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2024-10-23 10:42:15 -0400

    fix: sequalise

--- src/database/config/dbConfig.js
+++ src/database/config/dbConfig.js
@@ -6,10 +6,10 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: process.env.DB_USER || 'runner',
-        password: process.env.DB_PASSWORD || 'runner',
-        database: process.env.DB_NAME || 'postgres',
-        host: process.env.DB_HOST || 'localhost',
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
         dialect: 'postgres',
         port: process.env.DB_PORT //5432
     },
