da56c80561d61fd62c5058ffcad5e6ed5d2a5bcb
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2024-10-24 01:52:14 -0400

    fix: config

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -2,18 +2,32 @@ const { Sequelize } = require('sequelize');
 const config = require('./config/dbConfig.js');
 
 const env = process.env.NODE_ENV || 'development';
-const theschema = process.env.DB_SCHEMA;
 
 const dbConfig = config[env];
+if (!dbConfig) {
+    throw new Error(`No configuration found for environment: ${env}`);
+}
+
 
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
     host: dbConfig.host,
     dialect: dbConfig.dialect || 'postgres',
     port: dbConfig.port,
-    timezone: '+00:00'
+    timezone: '+00:00',
+    schema: dbConfig.schema,
+    logging: console.log
+
 })
 const connectToDatabase = async () => {
     try {
+        console.log('Attempting to connect with config:', {
+            database: dbConfig.database,
+            username: dbConfig.username,
+            host: dbConfig.host,
+            port: dbConfig.port,
+            schema: dbConfig.schema,
+            environment: env
+        });
         await sequelize.authenticate();
         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         console.log(`Connection to ${process.env.DB_NAME} DB is succesful.`);
@@ -30,6 +44,13 @@ const connectToDatabase = async () => {
     } catch (error) {
         console.log("***************************");
         console.error('Unable to connect to the database:', error);
+        console.log('Current environment:', env);
+        console.log('Database config:', {
+            host: dbConfig.host,
+            port: dbConfig.port,
+            database: dbConfig.database,
+            schema: dbConfig.schema
+        });
         console.log("***************************");
     }
 };
