e05636b2a2f6270ffb4b7685d8617e27e7222418
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2024-10-31 03:47:16 -0400

    fix: shellscripts

--- packer/setup-cloudwatch.sh
+++ packer/setup-cloudwatch.sh
@@ -20,8 +20,4 @@ echo "Setting up CloudWatch configuration directory..."
 sudo mkdir -p /opt/aws/amazon-cloudwatch-agent/etc
 sudo chown ubuntu:ubuntu /opt/aws/amazon-cloudwatch-agent/etc
 
-# Move the configuration file to the correct location
-echo "Moving CloudWatch configuration file..."
-mv /tmp/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
-
 echo "CloudWatch Agent setup complete. Agent is not started."
