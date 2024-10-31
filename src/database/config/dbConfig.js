ec37fdb3fbe28bb178c88e733c619ca24f9f4269
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2024-10-31 01:46:23 -0400

    fix: dbconfig

--- src/database/config/dbConfig.js
+++ src/database/config/dbConfig.js
@@ -5,15 +5,6 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        port: process.env.DB_PORT,
-        schema: process.env.DB_SCHEMA
-    },
-    test: {
         username: process.env.DB_USER || 'runner',
         password: process.env.DB_PASSWORD || 'runner',
         database: process.env.DB_NAME || 'postgres',
@@ -21,6 +12,15 @@ module.exports = {
         dialect: 'postgres',
         port: process.env.DB_PORT || 5432,
         schema: process.env.DB_SCHEMA || 'public'
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        schema: process.env.DB_SCHEMA
     },
     production: {
         username: process.env.DB_USER,
